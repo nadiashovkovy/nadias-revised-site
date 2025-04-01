abb38bd945ef0de09fc9b434b98d60c59db560c2
Nadia Shovkovy <nadiashovkovy@gmail.com>
2025-04-01 13:23:34 -0700

    added some UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import './styles.css';
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsVisible(true), 100); 
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header className="header">
+        <nav>
+            <ul>
+                <li><a href="#about"class="enlarge-text-on-hover">about</a></li>
+                <li><a href="#search"class="enlarge-text-on-hover">services</a></li>
+                <li><a href="#download"class="enlarge-text-on-hover">download</a></li>
+                <li><a href="#contact"class="enlarge-text-on-hover">contact</a></li>
+            </ul>
+        </nav>
       </header>
+
+      <section id="home" className="home">
+        <img src = "images/page1bg1.png" className = "bg"/>
+        <section id="home-content" className='home-content'> 
+          <img src = "images/welcomeimg.png" className="welcome-img" />
+        </section>
+      </section>
+
+
+      {/* <section id = "footer" className="footer"> 
+        <p className="footer-text">made with love by nadia shovkovy</p>
+      </section> */}
+
     </div>
   );
 }
